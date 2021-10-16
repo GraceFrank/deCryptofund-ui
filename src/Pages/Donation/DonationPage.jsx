607388ff81625fed58857d7f346d9ac124dee1e1
Grace <frankgracey@gmail.com>
2021-10-17 00:56:46 +0100

    added  donor list

--- src/Pages/Donation/DonationPage.jsx
+++ src/Pages/Donation/DonationPage.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import Nav from "../../components/Nav";
 import DonateCard from "./DonateCard";
 import DonateDetails from "./DonateDetails";
+import Donors from "./Donors";
 
 export default function DonatePage() {
   return (
     <>
       <Nav />
       <section id="pricing" class="pricing">
-        <div class="container" data-aos="fade-up">
+        <div class="container mt-5" data-aos="fade-up">
           <header class="section-header">
-            <h2>Pricing</h2>
-            <p>Check our Pricing</p>
+            <h2>Donation</h2>
+            <p>Help William Fight Cancer</p>
           </header>
           <div className="d-flex justify-content-between d-flex align-items-start">
-            <DonateDetails />
+            <div>
+              <DonateDetails />
+              <Donors />
+            </div>
 
             <DonateCard />
           </div>
---
+++ src/Pages/Donation/Donors.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default function Donors() {
+  return (
+    <div class="row feature-icons" data-aos="fade-up">
+      <h3>Recent Donations</h3>
+
+      <div class="row">
+        <div class="col-xl-8 d-flex content">
+          <div class="row align-self-center gy-4">
+            {/*  */}
+            <div class="col-md-12 icon-box service-box " data-aos="fade-up">
+              <i class="bi bi-suit-heart-fill"></i>
+              <div>
+                <h4>$100</h4>
+                <p>
+                  Consequuntur sunt aut quasi enim aliquam quae harum pariatur
+                  laboris nisi ut aliquip
+                </p>
+              </div>
+            </div>
+
+            {/*  */}
+            <div class="col-md-12 icon-box service-box " data-aos="fade-up">
+              <i class="bi bi-suit-heart-fill"></i>
+              <div>
+                <h4>$100</h4>
+                <p>
+                  Consequuntur sunt aut quasi enim aliquam quae harum pariatur
+                  laboris nisi ut aliquip
+                </p>
+              </div>
+            </div>
+
+            {/*  */}
+            <div class="col-md-12 icon-box service-box " data-aos="fade-up">
+              <i class="bi bi-suit-heart-fill"></i>
+              <div>
+                <h4>$100</h4>
+                <p>
+                  Consequuntur sunt aut quasi enim aliquam quae harum pariatur
+                  laboris nisi ut aliquip
+                </p>
+              </div>
+            </div>
+
+            {/*  */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
